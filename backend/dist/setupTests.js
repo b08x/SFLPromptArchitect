eeabc0938d65ce554fce42c223eb8d32f31daf47
Robert Pannick <rwpannick@gmail.com>
2025-08-26 02:41:17 -0400

    cleanup

--- backend/dist/setupTests.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Global test setup
-const globals_1 = require("@jest/globals");
-// Increase test timeout for integration tests
-globals_1.jest.setTimeout(30000);
--- backend/dist/types/express.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * @file express.ts
- * @description Express.js type extensions for authentication middleware integration
- */
-Object.defineProperty(exports, "__esModule", { value: true });
